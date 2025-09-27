5487301f0ba18a131308842b8bb587a1c69f4846
Nicolas Tran <nicolas.tran@gmx.de>
2025-09-27 13:44:13 +0200

    minor rename + format

--- python-server/server.py
+++ python-server/server.py
@@ -12,20 +12,24 @@ import tls13_parser
 
 # Create a class to define the server functions, derived from
 # test_pb2_grpc.GreeterServicer
-class GreeterServicer(tls13_pb2_grpc.TlsParserServicer):
+class TlsParserServicer(tls13_pb2_grpc.TlsParserServicer):
     def SayHello(self, request, context):
         print(f"Received request with data: {request.data}")
 
         # handle prefix
-        length_prefix = format(int(len(request.data) / 2), 'x').zfill(4)
-        handshake_prefix = "16" + "0303" + length_prefix # handshake message + TLS1.2 identification
+        length_prefix = format(int(len(request.data) / 2), "x").zfill(4)
+        handshake_prefix = (
+            "16" + "0303" + length_prefix
+        )  # handshake message + TLS1.2 identification
         prefixed_request_data = handshake_prefix + request.data
 
         # parse data
         # TODO make generic: New Session Ticket
-        parsed_data: TLS13NewSessionTicket = tls13_parser.parse_tls13(prefixed_request_data)
+        parsed_data: TLS13NewSessionTicket = tls13_parser.parse_tls13(
+            prefixed_request_data
+        )
         parsed_as_dict = dict(parsed_data[1][0].fields)
-        len_as_string = format(parsed_as_dict.get("msglen"), 'x').zfill(4)
+        len_as_string = format(parsed_as_dict.get("msglen"), "x").zfill(4)
         try:
             # TODO handle other list contents?
 
@@ -33,8 +37,6 @@ class GreeterServicer(tls13_pb2_grpc.TlsParserServicer):
             # contained in the parsed request but not in the response model
             for key in ["msgtype", "msglen", "noncelen", "ticketlen", "extlen", "ext"]:
                 del parsed_as_dict[key]
-
-
 
             # create response
             response_dict = dict(handshake=dict(new_session_ticket=parsed_as_dict))
@@ -45,15 +47,16 @@ class GreeterServicer(tls13_pb2_grpc.TlsParserServicer):
         except Exception as e:
             print(f"Error parsing {request.data}; error: {e}")
 
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 
     # Add the TlsParser service to the server
-    tls13_pb2_grpc.add_TlsParserServicer_to_server(GreeterServicer(), server)
+    tls13_pb2_grpc.add_TlsParserServicer_to_server(TlsParserServicer(), server)
 
     # Listen on port 50051
     print("Starting server. Listening on port 50051.")
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port("[::]:50051")
     server.start()
 
     # Keep the server running
@@ -63,5 +66,6 @@ def serve():
     except KeyboardInterrupt:
         server.stop(0)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     serve()
