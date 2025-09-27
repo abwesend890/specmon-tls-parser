64c6b40890a78d4898ec9c9a20bee6f93d5d89fe
Nicolas Tran <nicolas.tran@gmx.de>
2025-09-27 10:17:10 +0200

    init goclient

---
+++ README.md
@@ -0,0 +1,7 @@
+# dependencies to build go client
+## packages
+You can install the protobuf compiler with the following command:
+`sudo apt-get update && sudo apt-get install protobuf-compiler`
+
+## update path variable
+The following should be within your $PATH`$(go env GOPATH)/bin`
---
+++ go-client/client.go
@@ -0,0 +1,43 @@
+// go-client/client.go
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	// Import the generated Go code package
+	pb "go-client/Tls13Parser"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	// Create a new Greeter client
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// Call the SayHello RPC
+	name := "Go"
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+
+	log.Printf("Greeting from server: %s", r.GetMessage())
+}
