72c374dcbf3e3597031deb1f271c22510fb6d345
Nicolas Tran <nicolas.tran@gmx.de>
2025-10-26 16:12:17 +0100

    HelloRetryRequest API

--- python-server/server.py
+++ python-server/server.py
@@ -10,6 +10,7 @@ import time
 
 from loguru import logger
 from scapy.layers.tls.record import TLSChangeCipherSpec, TLSApplicationData
+from scapy.layers.tls.session import _GenericTLSSessionInheritance
 from scapy.packet import Raw
 
 import utils.logging_config  # noqa: F401
@@ -29,6 +30,7 @@ from scapy.layers.tls.handshake import (
     TLS13KeyUpdate,
     TLS13EndOfEarlyData,
     TLS13HelloRetryRequest,
+    TLSClientHello,
 )
 
 # Import the generated classes
@@ -76,23 +78,28 @@ def _format_fields(parsed, of_interest_mapping: dict):
 
 
 def _handle_handshake(
-    parsed_list: List[
-        Union[
-            TLS13ClientHello
-            | TLS13ServerHello
-            | TLS13NewSessionTicket
-            | TLS13Certificate
-            | TLS13CertificateRequest
-            | TLS13KeyUpdate
-            | TLS13EndOfEarlyData
-        ]
-    ],
+    parsed_list: List[_GenericTLSSessionInheritance],
 ):
 
     parse_response = list()
     for parsed in parsed_list:
         parse_response_message = dict()
         logger.debug(f"In handle Handshake with type {type(parsed)}")
+
+        #  HELLO RETRY REQUEST
+        if isinstance(parsed, TLS13HelloRetryRequest):
+            of_interest_scapy_mapping = dict(
+                version="legacy_version",
+                random_bytes="random",
+                sid="legacy_session_id_echo",
+                cipher="cipher_suites",
+                comp="legacy_compression_methods",
+                ext="extensions",
+            )
+            answer = _format_fields(parsed, of_interest_scapy_mapping)
+            parse_response_message["hello_retry_request"] = answer
+            parse_response.append(parse_response_message)
+            continue
 
         #  NEW SESSION TICKET
         if isinstance(parsed, TLS13NewSessionTicket):
@@ -176,34 +183,15 @@ class TlsParserServicer(tls13_pb2_grpc.TlsParserServicer):
         part1, part2 = data.split(pos)
         return tls13_pb2.SplitResponse(part1=part1, part2=part2)
 
-    def Parse(self, request, context):
+    def ParseHelloRetryRequest(self, request, context):
         logger.info(f"Received request with data: {request.data.hex()}")
 
         # prefix data in case we do not have equal number of bytes
         data = request.data
-        # if len(data) % 2 != 0:
-        #     logger.debug("Prefixing with '00'... ")
-        #     data = bytes.fromhex("00") + data
-
-        # length_prefix = format(int(len(request.data) / 2), "x").zfill(4)
-        # handshake message + TLS1.2 identification
-        # handshake_prefix = bytes.fromhex("16" + "0303" + length_prefix)
-        # prefixed_request_data = handshake_prefix + request.data
-        # try:
-        # parsed_data = tls13_parser.parse_tls13(
-        #     prefixed_request_data
-        # )
-
         try:
-            parsed_data = tls13_parser.parse_tls13(data)
-            # if len(parsed_data) != 1:
-            #     raise NotImplementedError(
-            #         "Attempting to parse exactly 1 message, else we'd need to return a list"
-            #     )
-            # parsed_data = parsed_data[0]
-            # str() returns a byte object on parsed_data
-            # logger.debug(f"Received parsed data: {parsed_data.__str__().hex()}")
-            # parsed_as_dict = dict(parsed_data[1][0].fields)
+            parsed_data = tls13_parser.parse_tls13_cached(
+                data, known_content_type=tls13_parser.ContentType.HelloRetryRequest
+            )
             return _handle_handshake(parsed_data)
 
         except AssertionError as e:
@@ -215,6 +203,65 @@ class TlsParserServicer(tls13_pb2_grpc.TlsParserServicer):
             context.set_code(grpc.StatusCode.INTERNAL)
             context.set_details(f"An error occurred: {e}")
             raise e
+
+    def Parse(self, request, context):
+        logger.info(f"Received request with data: {request.data.hex()}")
+
+        # prefix data in case we do not have equal number of bytes
+        data = request.data
+        try:
+            parsed_data = tls13_parser.parse_tls13_cached(data)
+            return _handle_handshake(parsed_data)
+
+        except AssertionError as e:
+            logger.exception(e)
+            raise e
+        except Exception as e:
+            logger.error(f"Error parsing {request.data}; error: {e}")
+            logger.exception(e)
+            context.set_code(grpc.StatusCode.INTERNAL)
+            context.set_details(f"An error occurred: {e}")
+            raise e
+
+    # def ParseDecryptedTODO(self):
+    #     logger.info(f"Received request with data: {request.data.hex()}")
+    #
+    #     # prefix data in case we do not have equal number of bytes
+    #     data = request.data
+    #     # if len(data) % 2 != 0:
+    #     #     logger.debug("Prefixing with '00'... ")
+    #     #     data = bytes.fromhex("00") + data
+    #
+    #     # length_prefix = format(int(len(request.data) / 2), "x").zfill(4)
+    #     # handshake message + TLS1.2 identification
+    #     # handshake_prefix = bytes.fromhex("16" + "0303" + length_prefix)
+    #     # prefixed_request_data = handshake_prefix + request.data
+    #     # try:
+    #     # parsed_data = tls13_parser.parse_tls13(
+    #     #     prefixed_request_data
+    #     # )
+    #
+    #     try:
+    #         parsed_data = tls13_parser.parse_tls13_cached(data)
+    #         # if len(parsed_data) != 1:
+    #         #     raise NotImplementedError(
+    #         #         "Attempting to parse exactly 1 message, else we'd need to return a list"
+    #         #     )
+    #         # parsed_data = parsed_data[0]
+    #         # str() returns a byte object on parsed_data
+    #         # logger.debug(f"Received parsed data: {parsed_data.__str__().hex()}")
+    #         # parsed_as_dict = dict(parsed_data[1][0].fields)
+    #         return _handle_handshake(parsed_data)
+    #
+    #     except AssertionError as e:
+    #         logger.exception(e)
+    #         raise e
+    #     except Exception as e:
+    #         logger.error(f"Error parsing {request.data}; error: {e}")
+    #         logger.exception(e)
+    #         context.set_code(grpc.StatusCode.INTERNAL)
+    #         context.set_details(f"An error occurred: {e}")
+    #         raise e
 
 
 def serve():
